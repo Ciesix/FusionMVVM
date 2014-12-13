127414594565892dfbaa8e8ae47a4db966e7dfca
Dennis Madsen <ddm@hardwick.dk>
2014-12-13 10:48:31 +0100

    Added basic RelayCommand

---
+++ FusionMVVM/Command/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FusionMVVM.Command
+{
+    public class RelayCommand : RelayCommandBase
+    {
+        private readonly Action _action;
+
+        /// <summary>
+        /// Initializes a new instance of the RelayCommand class.
+        /// </summary>
+        /// <param name="action"></param>
+        public RelayCommand(Action action)
+            : this(action, () => true)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the RelayCommand class.
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="canExecuteAction"></param>
+        public RelayCommand(Action action, Func<bool> canExecuteAction)
+            : base(canExecuteAction)
+        {
+            _action = action;
+        }
+
+        /// <summary>
+        /// Defines the method to be called when the command is invoked.
+        /// </summary>
+        /// <param name="parameter"></param>
+        public override void Execute(object parameter)
+        {
+            if (parameter == null) _action();
+        }
+    }
+}
---
+++ FusionMVVM/Command/RelayCommandBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace FusionMVVM.Command
+{
+    public abstract class RelayCommandBase : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private readonly Func<bool> _canExecuteAction;
+
+        /// <summary>
+        /// Initializes a new instance of the RelayCommandBase class.
+        /// </summary>
+        /// <param name="canExecuteAction"></param>
+        protected RelayCommandBase(Func<bool> canExecuteAction)
+        {
+            _canExecuteAction = canExecuteAction;
+        }
+
+        /// <summary>
+        /// Defines the method that determines whether the command can execute in its current state.
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter)
+        {
+            return _canExecuteAction == null || _canExecuteAction.Invoke();
+        }
+
+        /// <summary>
+        /// Defines the method to be called when the command is invoked.
+        /// </summary>
+        /// <param name="parameter"></param>
+        public abstract void Execute(object parameter);
+
+        /// <summary>
+        /// Raises the CanExecuteChanged event on the command.
+        /// </summary>
+        public virtual void OnCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null) handler(this, EventArgs.Empty);
+        }
+    }
+}
