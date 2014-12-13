d527f8ab94e62f6cc60340e66a9d4f7c7df9f456
Dennis Madsen <ddm@hardwick.dk>
2014-12-13 14:25:17 +0100

    Execute a commad with or without a parameter

--- FusionMVVM/Command/RelayCommand.cs
+++ FusionMVVM/Command/RelayCommand.cs
@@ -5,6 +5,7 @@ namespace FusionMVVM.Command
     public class RelayCommand : RelayCommandBase
     {
         private readonly Action _action;
+        private readonly Action<object> _actionParameter;
 
         /// <summary>
         /// Initializes a new instance of the RelayCommand class.
@@ -27,12 +28,43 @@ namespace FusionMVVM.Command
         }
 
         /// <summary>
+        /// Initializes a new instance of the RelayCommand class.
+        /// </summary>
+        /// <param name="action"></param>
+        public RelayCommand(Action<object> action)
+            : this(action, () => true)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the RelayCommand class.
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="canExecuteAction"></param>
+        public RelayCommand(Action<object> action, Func<bool> canExecuteAction)
+            : base(canExecuteAction)
+        {
+            _actionParameter = action;
+        }
+
+        /// <summary>
         /// Defines the method to be called when the command is invoked.
         /// </summary>
         /// <param name="parameter"></param>
         public override void Execute(object parameter)
         {
-            if (parameter == null) _action();
+            if (_action != null && parameter == null)
+            {
+                _action();
+            }
+            else if (_actionParameter != null && parameter != null)
+            {
+                _actionParameter(parameter);
+            }
+            else
+            {
+                throw new InvalidOperationException("The executing method was not found.");
+            }
         }
     }
 }
