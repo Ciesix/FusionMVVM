b8e4c6146ee5ad5c6153ddb7b1ad7dda1e32df64
Dennis Madsen <ddm@hardwick.dk>
2015-06-18 08:10:30 +0200

    Null parameter check on the Subscriber class

---
+++ FusionMVVM.Tests/Common/SubscriberTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using FusionMVVM.Common;
+using Ploeh.AutoFixture;
+using Xunit;
+
+namespace FusionMVVM.Tests.Common
+{
+    public class SubscriberTests
+    {
+        [Fact]
+        public void ConstructorShouldThrowExceptionWhenActionNull()
+        {
+            // Verify outcome.
+            Assert.Throws<ArgumentNullException>(() => new Subscriber(null));
+        }
+
+        [Fact]
+        public void InvokeShouldThrowExceptionWhenMessageNull()
+        {
+            // Fixture setup.
+            var fixture = new Fixture();
+            var action = fixture.Create<Action>();
+            var sut = new Subscriber(action);
+
+            // Verify outcome.
+            Assert.Throws<ArgumentNullException>(() => sut.Invoke<object>(null));
+        }
+    }
+}
--- FusionMVVM/Common/Subscriber.cs
+++ FusionMVVM/Common/Subscriber.cs
@@ -4,12 +4,23 @@ namespace FusionMVVM.Common
 {
     public class Subscriber
     {
+        /// <summary>
+        /// Initializes a new instance of the Subscriber class.
+        /// </summary>
+        /// <param name="action"></param>
         public Subscriber(Delegate action)
         {
+            if (action == null) throw new ArgumentNullException("action");
         }
 
+        /// <summary>
+        /// Invokes the action associated with the subscriber.
+        /// </summary>
+        /// <typeparam name="TEvent"></typeparam>
+        /// <param name="message"></param>
         public void Invoke<TEvent>(TEvent message)
         {
+            if (message == null) throw new ArgumentNullException("message");
         }
     }
 }
